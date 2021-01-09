37708c0b2d7e5ddd93181806feea46497142d1f9
HJtime <72139168+HJtime@users.noreply.github.com>
2021-01-09 23:44:53 +0900

    JS skill animation

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ navbarToggle.addEventListener('click',()=>{
     navbarMenu.classList.toggle('active');
     navbar.classList.toggle('active');
 });
+
+// Skill 그래프 애니메이션
+const skillvalue=document.querySelector('.skill__value');
+const about=document.querySelector('#about');
+const aboutHeight=about.getBoundingClientRect().height;
+const skillvalues=document.querySelectorAll('.skill__value');
+
+window.addEventListener('scroll', ()=>{
+    const scrolling=window.scrollY;
+
+    if(scrolling > aboutHeight){
+        skillvalues.forEach((skillvalues)=>{
+            const width=skillvalues.dataset.value;
+            skillvalues.style.width=width+'%';
+        });
+    }
+});
